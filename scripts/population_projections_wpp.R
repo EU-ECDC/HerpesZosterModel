342a25f919adb85512dfacac73474332b5cb9f4b
Maria <33788128+mariabnd@users.noreply.github.com>
2018-10-12 15:09:57 +0200

    Fixed parts where incorrect dataset was being used

--- scripts/population_projections_wpp.R
+++ scripts/population_projections_wpp.R
@@ -25,7 +25,8 @@ ggplot(data = data,
        mapping = aes(x = Time, colour = Variant,
                      y = Population, linetype = Sex)) + 
 geom_smooth(method = "gam", formula = y ~ s(x, bs = "cs"), se = FALSE) +
-  facet_wrap(. ~ Location, scales = "free_y")
+  facet_wrap(. ~ Location, scales = "free_y") +
+  scale_colour_viridis(option = "A", discrete = TRUE)
 
 # Load UN WPP data by age
 file <- "https://population.un.org/wpp/DVD/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2017_PopulationByAgeSex_Medium.csv"
@@ -40,8 +41,7 @@ data <- data %>%
   filter(LocID %in% c(40, 56, 100, 191, 196, 203, 208, 233, 246, 250, 276, 300,
                       348, 352, 372, 380, 428, 438, 440, 442, 470, 528, 578, 616,
                       620, 642, 703, 705, 724, 752, 826)) %>%
-  gather(Sex, Population, PopMale:PopTotal, factor_key = TRUE) %>%
-  filter(Sex != "PopTotal")
+  gather(Sex, Population, PopMale:PopTotal, factor_key = TRUE)
 # Turn age group into factor
 data$AgeGrp <- factor(data$AgeGrp,
                       levels = c("0-4", "5-9", "10-14", "15-19", "20-24", "25-29",
@@ -50,7 +50,7 @@ data$AgeGrp <- factor(data$AgeGrp,
                                  "80+", "80-84", "85-89", "90-94", "95-99", "100+"))
 
 # Plot pyramids over time (three snapshots)
-p1 <- ggplot(data = data %>% filter(Time == 2015), 
+p1 <- ggplot(data = data %>% filter(Time == 2015) %>% filter(Sex == "PopTotal"), 
              mapping = aes(x = AgeGrp, fill = Sex, 
                            y = ifelse(test = Sex == "PopMale", 
                                       yes = -Population, no = Population),
@@ -63,7 +63,7 @@ p1 <- ggplot(data = data %>% filter(Time == 2015),
   scale_colour_viridis(option = "A", discrete = TRUE) +
   scale_fill_grey(start = 0.6, end = 0.4) +
   guides(colour = guide_legend(override.aes = list(fill = NA)))
-p2 <- ggplot(data = data %>% filter(Time == 2050), 
+p2 <- ggplot(data = data %>% filter(Time == 2050) %>% filter(Sex == "PopTotal"), 
              mapping = aes(x = AgeGrp, fill = Sex, 
                            y = ifelse(test = Sex == "PopMale", 
                                       yes = -Population, no = Population),
@@ -76,7 +76,7 @@ p2 <- ggplot(data = data %>% filter(Time == 2050),
   scale_colour_viridis(option = "A", discrete = TRUE) +
   scale_fill_grey(start = 0.6, end = 0.4) +
   guides(colour = guide_legend(override.aes = list(fill = NA)))
-p3 <- ggplot(data = data %>% filter(Time == 2100), 
+p3 <- ggplot(data = data %>% filter(Time == 2100) %>% filter(Sex == "PopTotal"), 
              mapping = aes(x = AgeGrp, fill = Sex, 
                            y = ifelse(test = Sex == "PopMale", 
                                       yes = -Population, no = Population),
@@ -91,7 +91,36 @@ p3 <- ggplot(data = data %>% filter(Time == 2100),
   guides(colour = guide_legend(override.aes = list(fill = NA)))
 grid.arrange(p1, p2, p3, ncol = 3)
 
-# Indicators
+# Plot changes in age group 0-4 over time
+ggplot(data = left_join(data %>%
+                          filter(Sex == "PopTotal") %>%
+                          filter(AgeGrp == "0-4") %>%
+                          group_by(Location, Variant),
+                        data %>%
+                          filter(Sex == "PopTotal") %>%
+                          filter(AgeGrp == "0-4", Time == 2015) %>%
+                          group_by(Location, Variant) %>%
+                          mutate(Subtr = Population)) %>%
+         select(Time, Location, Variant, Population, Subtr) %>%
+         mutate(Time =  factor(Time, levels = c(2015,
+                                                seq(1950, 2015 - 1, 1),
+                                                seq(2015 + 1, 2100, 1)))) %>%
+         group_by(Location, Variant) %>%
+         arrange(Time) %>%
+         fill(Subtr, Subtr) %>%
+         mutate(Time = factor(Time, levels = c(seq(1950, 2100, 1)))) %>%
+         mutate (PopDiff = Population - Subtr),
+       mapping = aes(x = Time, y = PopDiff,
+                     colour = Location, group = Location)) +
+  geom_line(aes(y = PopDiff)) +
+  labs(title = "Changes from 2015 values", subtitle = "Population aged 0-4") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  facet_grid(. ~ Variant) +
+  scale_colour_viridis(discrete = TRUE, option = "C") +
+  scale_x_discrete(breaks = c("1950", "2000", "2015", "2050", "2100"),
+                   labels = c("1950", "2000", "2015", "2050", "2100"))
+
+# Load indicators
 file <- "https://population.un.org/wpp/DVD/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2017_Period_Indicators_Medium.csv"
 med <- read.csv(file, header = TRUE, na.strings = "NULL")
 file <- "https://population.un.org/wpp/DVD/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2017_Period_Indicators_OtherVariants.csv"
@@ -116,8 +145,8 @@ data <- data %>%
   gather(Sex, Population, PopMale:PopTotal, factor_key = TRUE) %>%
   filter(Sex == "PopTotal")
 
-ggplot(data = data, 
-
+# Plot indicators
+ggplot(data = data,
        mapping = aes(x = Time,
                      y = NetMigrations,
                      group = Variant, colour = Variant)) +
@@ -146,38 +175,3 @@ ggplot(data = data,
   labs(title = "Population growth rate") +
   theme(axis.text.x  = element_text(angle = 90, vjust = 1)) +
   scale_colour_viridis(discrete = TRUE)
-
-# Plot changes in age group 0-4 over time
-data <- full_join(med, others)
-data <- data %>%
-  filter(LocID %in% c(40, 56, 100, 191, 196, 203, 208, 233, 246, 250, 276, 300,
-                      348, 352, 372, 380, 428, 438, 440, 442, 470, 528, 578, 616,
-                      620, 642, 703, 705, 724, 752, 826)) %>%
-  gather(Sex, Population, PopMale:PopTotal, factor_key = TRUE) %>%
-  filter(Sex == "PopTotal")
-
-ggplot(data = left_join(data %>%
-                          filter(AgeGrp == "0-4") %>%
-                          group_by(Location, Variant),
-                        data %>%
-                          filter(AgeGrp == "0-4", Time == 2015) %>%
-                          group_by(Location, Variant) %>%
-                          mutate(Subtr = Population)) %>%
-         select(Time, Location, Variant, Population, Subtr) %>%
-         mutate(Time =  factor(Time, levels = c(2015,
-                                                seq(1950, 2015 - 1, 1),
-                                                seq(2015 + 1, 2100, 1)))) %>%
-         group_by(Location, Variant) %>%
-         arrange(Time) %>%
-         fill(Subtr, Subtr) %>%
-         mutate(Time = factor(Time, levels = c(seq(1950, 2100, 1)))) %>%
-         mutate (PopDiff = Population - Subtr),
-       mapping = aes(x = Time, y = PopDiff,
-                     colour = Location, group = Location)) +
-  geom_line(aes(y = PopDiff)) +
-  labs(title = "Changes from 2015 values", subtitle = "Population aged 0-4") +
-  theme(axis.text.x = element_text(angle = 90)) +
-  facet_grid(. ~ Variant) +
-  scale_colour_viridis(discrete = TRUE, option = "C") +
-  scale_x_discrete(breaks = c("1950", "2000", "2015", "2050", "2100"),
-                     labels = c("1950", "2000", "2015", "2050", "2100"))
