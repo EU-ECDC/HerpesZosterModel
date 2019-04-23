5e47a36fc934da916ba8406e2d34af362a89be50
mariabnd <33788128+mariabnd@users.noreply.github.com>
2019-04-23 17:37:30 +0200

    Added seroprevalence points to plots

--- R/results.R
+++ R/results.R
@@ -17,17 +17,26 @@ while(i <= dim(opts)[1]){
               N = sum(PS), D = 6 / 365, A = 0.5, Lmax = 70, 
               prop = "loglin", startpar = c(0.5, 0.3))
   
+  # Plot results
   plot(res1$lambda, ylim = c(0, 1), xlab = "age", ylab = "", type = "l", col = 4)
   points(unique(cbind(sero$AGE, res1$pi)), type = "l", col = 4)
   text(length(res1$lambda), tail(res1$lambda, 1) + 0.05, "FOI")#, col = 4)
   text(max(sero$AGE), tail(res1$pi, 1) - 0.05, "prev")#, col = 4)
-  if(!(opts[i, 3] %in% c("FI", "UK", "RS"))){
-    # Currently experiencing FOI issues for Finland, Serbia, UK
-    points(res2$lambda, ylim = c(0, 1), xlab = "age", ylab = "", type = "l")
-    points(unique(cbind(sero$AGE, res2$pi)), type = "l")
-    #text(length(res2$lambda), tail(res2$lambda, 1) + 0.05, "FOI")
-    #text(max(sero$AGE), tail(res2$pi, 1) - 0.05, "prev")
-  }
+  points(res2$lambda, ylim = c(0, 1), xlab = "age", ylab = "", type = "l")
+  points(unique(cbind(sero$AGE, res2$pi)), type = "l")
+  # Add seroprevalence points
+  subset <- (sero$AGE > 0.5) & (sero$AGE < 80) &
+    (!is.na(sero$AGE)) & !is.na(sero$indic)
+  sero <- sero[subset, ]
+  y <- sero$indic[order(sero$AGE)]
+  a <- sero$AGE[order(sero$AGE)]
+  
+  grid <- sort(unique(round(a)))
+  neg <- table(y, round(a))[1, ]
+  pos <- table(y, round(a))[2, ]
+  tot <- neg + pos
+  points(grid, pos / tot, cex = 0.02 * tot)
+  # Add right axis, legend, and title
   axis(4, at = pretty(range(c(res1$lambda, res2$lambda))))
   legend("topright", col = c(1, 4), c("Log-linear", "Constant"), lty = 1)
   title(main = opts[i, 1])
@@ -37,3 +46,19 @@ while(i <= dim(opts)[1]){
   i <- i + 1
 }
 dev.off()
+
+if(FALSE){ #TODO ggplot2 version
+  ggplot() +
+    geom_point(data = as.data.frame(cbind(grid, neg, pos, tot)), 
+               mapping = aes(x = grid, y = pos / tot, size = 0.02 * tot),
+               pch = 1) +
+    geom_line(data = data.frame(age = 1 : length(res1$lambda), lambda = res1$lambda),
+              mapping = aes(x = age, y = lambda), colour = 4) +
+    geom_line(data = data.frame(unique(cbind(sero$AGE, res1$pi))),
+              mapping = aes(x = X1, y = X2), colour = 4) +
+    geom_line(data = data.frame(age = 1 : length(res2$lambda), lambda = res1$lambda),
+              mapping = aes(x = age, y = lambda), colour = 1) +
+    geom_line(data = data.frame(unique(cbind(sero$AGE, res2$pi))),
+              mapping = aes(x = X1, y = X2), colour = 1) +
+    labs(title = opts[i, 1])
+}
