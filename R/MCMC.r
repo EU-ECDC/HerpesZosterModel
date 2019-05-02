98972b396a3e8a9b3ff98dbebd8f959d486f7c27
hcb101 <helen.johnson@ecdc.europa.eu>
2019-05-02 08:00:50 +0200

    Update MCMC.r
    - Further integration of zoster model and MCMC routine

--- R/MCMC.r
+++ R/MCMC.r
@@ -10,34 +10,23 @@
 ## Initialisation ##
 ####################
 
-seroData <- sero # remove this when Maria has finished editing load_data
-
+			
+muy <- predict(demfit, type = "response")
+muy <- muy[1 : Lmax] # Ensure not longer than life expectancy
+  My <- exp(- cumsum(muy)) # Type I mortality
+  L <- Lmax * mean(My) # Life expectancy
+  My <- My[1 : Lmax] # Ensure no longer than life expectancy
+ 
+ 
 ## Group fixed parameters 
 otherParams <- list(rij = contact_w,     # country-specific contact matrix
-					N <- sum(popSize),   # population size
-					D <- 6 / 365,        # duration of infection?!
-					A <- 0.5,            # duration of maternally-derived immunity
-					propFac <- "loglin", # type of proportionality factor
-					Lmax <- 80           # life expectancy
+					N = nrow(seroData),  # sum(popSize),   # population size
+					D = 6 / 365,        # duration of infection?!
+					A = 0.5,            # duration of maternally-derived immunity
+					propFac = "constant", # type of proportionality factor
+					Lmax = 80           # life expectancy
 					)
  
-## Initialise MCMC params
-nEstimate <- 1               # no. of parameters to be estimated 
-nIter <- 1e4                 # number of iterations per chain
-prior <- c(1e-6, 1e3)        # set uniform prior for q params
-dispProp <-  1e-6 * prior[2] # dispersal parameter (inc. scaling coefficient)
-acc <- 0                     # Initialise acceptance
-
-mcmc.out <- rep(NA, (nEstimate+1)*nIter)  # Initialise output
-dim(mcmc.out) <- c(nIter,(nEstimate+1))
-
-######################
-## Initialise chain ##
-######################
-
-param0 <- c(0.5, 0.3) # initial values for q parameters
-lnLike0 <- FoI(param0, otherParams, seroData)$lnLike # Initialise log-likelihood
-
 #####################################
 ## Estimate force of infection     ##
 ## and calculate associated R0, R, ##
@@ -51,6 +40,14 @@ age     <- seroData$AGE
 obsData <- seroData$indic
 
 ## Generate transmission matrix
+    if(propFac == "constant"){
+      if(length(param0) != 1){
+        stop("length of param0 does not fit choice of constant proportionality factor")
+      }
+      # Create transmission matrix with constant parameter
+      bij <- 365 * fitParams[1] * (rij)[1 : Lmax, 1 : Lmax]
+    }
+	
     if(propFac == "loglin"){ # First option (age-dependent susceptibility)
      
 		# Check start parameters fit with prop. factor type
@@ -59,9 +56,9 @@ obsData <- seroData$indic
 		}
 	  
 		# Create transmission matrix with log-linear parameters
-		qFunction <- function(x, obsData){exp(fitParams[1] + fitParams[2] * x)}
+		qFunction <- function(x, obsData){exp(fitParams[1] + (fitParams[2] * x))}
 		qij <- outer(c(1 : Lmax), c(1 : Lmax), qFunction)
-		bij <- 365 * qij * rij)[1 : Lmax, 1 : Lmax]
+		bij <- 365 * qij * rij[1 : Lmax, 1 : Lmax]
 		}
 	if(sum(is.na(bij)) > 0){warning("There are missing values in the transmission matrix beta_ij, which may break the tolerance parameter in the optimisation")}
 
@@ -90,15 +87,17 @@ obsData <- seroData$indic
 	
 ## Initialise modelled prevalence
 	prev <- rep(NA, length(age))
+	ll <- rep(NA, length(age)) # initialise log-like
 	
 ## Estimate seroprevalence from fitted FoI model
-	for(i in 1 : length(age)){
-		# pi(a) = 1 - f(a)
-		prev[i] <- (1 - exp(c(- (age[i] - A) * foii[1],
-                            - (1 - A) * foii[1] - cumsum(c(0, foii[- 1])) -
-                            (foii[- 1])[floor(age[i])] * (age[i] - floor(age[i])))))[floor(age[i]) + 1]
+	for(k in 1 : length(age)){
+        prev[k] <- (1 - exp(c(- (age[k] - A) * foii[1],
+                      - (1 - A) * foii[1] - cumsum(c(0, foii[- 1]))
+					  - (foii[- 1])[floor(age[k])] * (age[k] - floor(age[k])))))[floor(age[k]) + 1]
+      # Bernoulli loglikelihood used to estimate FOI
+      ll[k] <- obsData[k] * log(prev[k]) + (1 - obsData[k]) * log(1 - prev[k])
     }
-	
+    
 ## Estimate R0 from next-generation matrix
     R0ij <- (N / L) * D * bij[1 : Lmax, 1 : Lmax]
     Mij <- diag(c(My[1 : Lmax])) # g(a) (see notes)
@@ -115,11 +114,9 @@ obsData <- seroData$indic
     len <- min(dim(NGM)[1], length(suscp))
     Rvec <- NGM[1 : len, 1 : len] %*% diag(suscp[1 : len])
 	R <- max(as.double(Rvec)) # spectral radius
-	
-## Calculate log-likelihood
-	ll[i] <- obsData[i] * log(prev[i] + 1e-8) + (1 - obsData[i]) * log(1 - prev[i] + 1e-8) # This is currently done piecewise. Could use seroTab for grouping.
-    
-	return(list(lnLike = ll,     # log-likelihood
+
+    ll[!is.finite(ll)] <- 0 
+	return(list(lnLike =  2 * sum(ll),     # log-likelihood
 			    R0 = R0,         # R0
 				R = R,           # R
 				prev = prev,     # estimated seroprevalence
@@ -129,17 +126,38 @@ obsData <- seroData$indic
 ###################
 ## MCMC function ##
 ###################
-ptm <- proc.time()[3]
+
+## Initialise MCMC params(
+nEstimate <- 1                  # no. of parameters to be estimated 
+nIter <- 1e3                    # number of iterations per chain
+prior <- c(0,0.1) #(c(0,1), c(-1,1)) # set uniform prior for q params
+dim(prior) <- c(2,nEstimate)
+prior <- t(prior) 
+scaleDisp <- 1e-3				# set scaling factor for dispersion           
+dispProp <-  diag(nEstimate) * scaleDisp * (prior[,2]-prior[,1]) # dispersal parameter
+acc <- 0                        # Initialise acceptance
+
+mcmc.out <- rep(NA, (nEstimate+1)*nIter)  # Initialise output
+dim(mcmc.out) <- c(nIter,(nEstimate+1))
+
+######################
+## Initialise chain ##
+######################
+param0 <- 0.05
+#param0 <- c(0.5, 0.5) # initial values for q parameters
+lnLike0 <- FoI(param0, otherParams, seroData)$lnLike # Initialise log-likelihood
+
+ptm <- proc.time()[3] # set clock
 for(i in 1:nIter){
 
 	param1 <- drop(rmvnorm(1, mean=param0, sigma=dispProp))
 
-	## Check what this is doing? Bouncing?
+	## Check what this is doing? Bouncing? Yes
 	for(j in 1:length(prior[,1])){
 		param1[j] <- ifelse(param1[j] > prior[j,2], prior[j,2]-((param1[j]-prior[j,2])%%(prior[j,2]-prior[j,1])), ifelse(param1[j] < prior[j,1], prior[j,1]+((prior[j,1]-param1[j])%%(prior[j,2]-prior[j,1])), param1[j]))
 	}
 
-	lnLike1 <- FoI(param1, otherParams, seroData)$ll
+	lnLike1 <- FoI(param1, otherParams, seroData)$lnLike
 
 	if(!is.na(lnLike1)){
 		logAlpha <- (lnLike1 + dmvnorm(param0, param1, dispProp, log=T)) - (lnLike0 + dmvnorm(param1, param0, dispProp, log=T)) 
@@ -167,8 +185,8 @@ for(i in 1:nIter){
 cat("\nAlgorithm required", (proc.time()[3]-ptm)/60, "minutes\nwith final acceptance rate", round(acc.rate, digits=2), "%\n")
 
 mcmc.out <- as.mcmc(mcmc.out) 
-HPV.res <- window(mcmc.out, start=1001)
-#plot(HPV.res)
+HPV.res <- window(mcmc.out, start=1)
+plot(HPV.res)
 ESS <- effectiveSize(HPV.res)
 sampleOutput <- HPV.res [sample(nrow(HPV.res ), 25, replace=TRUE),1:29]
 
