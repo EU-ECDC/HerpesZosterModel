479a859ce792c44db8c14a041f3ae60a1e20b585
mariabnd <33788128+mariabnd@users.noreply.github.com>
2019-05-07 16:22:16 +0200

    Add changes to load_data.R from additional_plots branch as these are independent of ongoing work on the model

--- R/load_data.R
+++ R/load_data.R
@@ -11,8 +11,8 @@ library(eurostat) # Data
 library(akima) # For interpolation
 library(gridExtra) # For plotting
 library(ggplot2) # For plotting
-theme_set(theme_classic() %+replace%
-            theme(plot.title = element_text(hjust = 0.5))) # Ensure centred titles
+library(lemon) # For shared legend
+theme_set(theme_classic())
 library(voxel) # For plotGAM
 
 # Load data
@@ -179,7 +179,7 @@ get_data <- function(code){
     weigh <- function(x, dat, mat){
       AGE <- droplevels(dat$age)
       levels(AGE)[length(levels(AGE))] <- "0.5"
-      AGE <- gsub("[A-z]","", AGE)
+      AGE <- gsub("[A-z]", "", AGE)
       AGE <- as.numeric(AGE)
       # Ensure both follow same age pattern
       AGE <- sort(AGE, index.return = TRUE)$`x`
@@ -240,7 +240,7 @@ get_data <- function(code){
     weigh <- function(x, dat, mat){
       AGE <- droplevels(dat$age)
       levels(AGE)[length(levels(AGE))] <- "0.5"
-      AGE <- gsub("[A-z]","", AGE)
+      AGE <- gsub("[A-z]", "", AGE)
       AGE <- as.numeric(AGE)
       AGE <- sort(AGE, index.return = TRUE)$`x`
       dat <- dat[sort(AGE, index.return = TRUE)$ix, ]$values
@@ -264,7 +264,7 @@ get_data <- function(code){
     weigh <- function(x, dat, mat){
       AGE <- droplevels(dat$age)
       levels(AGE)[length(levels(AGE))] <- "0.5"
-      AGE <- gsub("[A-z]","", AGE)
+      AGE <- gsub("[A-z]", "", AGE)
       AGE <- as.numeric(AGE)
       AGE <- sort(AGE, index.return = TRUE)$`x`
       dat <- dat[sort(AGE, index.return = TRUE)$ix, ]$values
@@ -310,9 +310,9 @@ use <- c(countries$code[countries$name %in%
 plot_mort <- function(code, ...){
   get_data(code)
   plotGAM(gamFit = demfit, smooth.cov = "mortAge") +
-    labs(title = code) +
-    theme_classic() + 
-    theme(plot.title = element_text(hjust = 0.5))
+    labs(title = code, y = "Predicted deaths", x = "Age (smoothed)") +
+    theme_classic() + theme(plot.title = element_text(hjust = 0.5),
+          title = element_text(family = "serif"))
 }
 
 # Save list of plots for the countries in use
@@ -321,10 +321,27 @@ plot_list <- lapply(use, plot_mort)
 lapply(seq_along(1 : length(use)), function(x){assign(use[x], plot_list[[x]], 
                                                       envir = .GlobalEnv)})
 
+no_axes <- theme(#axis.line = element_blank(),
+                 axis.text.x = element_blank(),
+                 axis.text.y = element_blank(),
+                 #axis.ticks = element_blank(),
+                 axis.title.x = element_blank(),
+                 axis.title.y = element_blank())
+
+no_x_axis <- theme(axis.text.x = element_blank(),
+                   axis.title.x = element_blank())
+
+no_y_axis <- theme(axis.text.y = element_blank(),
+                   axis.title.y = element_blank())
+
 tiff(filename = "S:/HelenJohnson/Herpes Zoster/Figures/mortality.tif",
      width = 800, height = 600)
 # Current options based on availability of data
-grid.arrange(BE, FI, DE, IE, IT, LU, NL, SK, UK, RS, SI)
+grid.arrange(BE + no_x_axis, FI + no_axes, DE + no_axes, 
+             IE + no_x_axis, IT + no_axes, LU + no_axes,
+             NL + no_x_axis, SK + no_axes, UK + no_axes, 
+             RS, SI + no_y_axis,
+             ncol = 3, nrow = 4)
 # TODO see if we can replace ^ with use somehow
 while(!is.null(dev.list())) dev.off()
 
@@ -334,9 +351,13 @@ plot_death <- function(code, ...){
   ggplot(mapping = aes(x = age, y = rate), 
          data = as.data.frame(cbind(age = 1 : length(popSize),
                                     rate = demfit$model$nDeaths / popSize * 1e+05))) + 
-    labs(x = "Age", y = "Deaths per 100000", title = code) +
+    labs(x = "Age", y = "Deaths per 100,000", title = code) +
     geom_line() +
-    theme(plot.title = element_text(hjust = 0.5))
+    theme(plot.title = element_text(hjust = 0.5), # Ensure centred titles
+          title = element_text(family = "serif")) +
+    scale_y_continuous(breaks = seq(5000, 5e04, 5000),
+                       labels = seq(5000, 5e04, 5000)) + 
+    ylim(0, 5e04)
 }
 
 # Save list of plots for the countries in use
@@ -348,7 +369,11 @@ lapply(seq_along(1 : length(use)), function(x){assign(use[x], plot_list[[x]],
 tiff(filename = "S:/HelenJohnson/Herpes Zoster/Figures/deaths.tif",
      width = 800, height = 600)
 # Current options based on availability of data
-grid.arrange(BE, FI, DE, IE, IT, LU, NL, SK, UK, RS, SI)
+grid.arrange(BE + no_x_axis, FI + no_axes, DE + no_axes, 
+             IE + no_x_axis, IT + no_axes, LU + no_axes,
+             NL + no_x_axis, SK + no_axes, UK + no_axes, 
+             RS, SI + no_y_axis,
+             ncol = 3, nrow = 4)
 while(!is.null(dev.list())) dev.off()
 
 ## Plot serological data
@@ -365,8 +390,14 @@ plot_sero <- function(code, ...){
                                     tot = rowSums(htab))),
          mapping = aes(x = age, y = prop, size = tot)) +
     geom_point(pch = 1) + 
-    labs(x = "age", y = "sero-prevalence", title = code) + 
-    xlim(0, 72) + ylim(- 0.1, 1) + theme(legend.title = element_blank())
+    labs(x = "Age", y = "Sero-prevalence", title = code) + 
+    xlim(0, 72) + ylim(- 0.1, 1) + 
+    theme(legend.title = element_blank()) +
+    scale_size_continuous(limits = c(1, 500),
+                          breaks=c(50, 100, 150, 200,
+                                   250, 300, 400, 500)) +
+    theme(plot.title = element_text(hjust = 0.5), # Ensure centred titles
+          title = element_text(family = "serif"))
 }
 
 # Save list of plots and assign to the environment
@@ -375,9 +406,13 @@ lapply(seq_along(plot_list), function(x){assign(use[x], plot_list[[x]],
                                                 envir = .GlobalEnv)})
 
 tiff(filename = "S:/HelenJohnson/Herpes Zoster/Figures/serology.tif",
-     width = 800, height = 600)
+     width = 800, height = 800)
 # Current options based on availability of data
-grid.arrange(BE, FI, DE, IE, IT, LU, NL, SK, UK, RS, SI)
+grid_arrange_shared_legend(BE + no_x_axis, FI + no_axes, DE + no_axes, 
+                           IE + no_x_axis, IT + no_axes, LU + no_axes,
+                           NL + no_x_axis, SK + no_axes, UK + no_axes, 
+                           RS, SI + no_y_axis,
+                           ncol = 3, nrow = 4)
 while(!is.null(dev.list())) dev.off()
 
 ## Plot contact rates
@@ -385,33 +420,39 @@ while(!is.null(dev.list())) dev.off()
 plot_mat <- function(code, ...){
   get_data(code)
   tmp <- melt(contact_w)
-  p <- ggplot(data = tmp, aes_string(x = names(tmp)[1], y = names(tmp)[2], 
+  # Relabel socialmixr for panel plot
+  if(is.factor(tmp$age.group)){
+    levels(tmp$age.group) <- 1 : length(levels(tmp$age.group))
+    tmp$age.group <- as.numeric(levels(tmp$age.group))[tmp$age.group]
+  }
+  if(is.factor(tmp$contact.age.group)){
+    levels(tmp$contact.age.group) <- 1 : length(levels(tmp$contact.age.group))
+    tmp$contact.age.group <- as.numeric(levels(tmp$contact.age.group))[tmp$contact.age.group]
+  }
+  ggplot(data = tmp, aes_string(x = names(tmp)[1], y = names(tmp)[2], 
                                 fill = names(tmp)[3])) + 
     geom_tile() + 
-    scale_fill_viridis_c(direction = - 1, option = "E", ...) +
-    labs(x = "", y = "", title = code) +
+    scale_fill_viridis_c(direction = - 1, option = "E", 
+                         guide = guide_legend(label.theme = element_text(angle = 90)),
+                         ...) +
+    labs(x = "", y = "", title = code, fill = "Rate") +
+    theme(plot.title = element_text(hjust = 0.5), # Ensure centred titles
+          title = element_text(family = "serif")) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-    # Rotate x-axis labels
-    if(is.factor(tmp[, 1])){ # Add spacing
-      scale_x_discrete(breaks = levels(tmp[, 1])[c(TRUE, rep(FALSE, 9))])
-    } else {
-      NULL
-    }
-  p <- p +
-    if(is.factor(tmp[, 2])){ # Add spacing
-      scale_y_discrete(breaks = levels(tmp[, 2])[c(TRUE, rep(FALSE, 9))])
-    } else {
-      NULL
-    }
-  return(p)
+    xlim(0, 100) + ylim(0, 100)
 }
 plot_list <- lapply(use, plot_mat)
 lapply(seq_along(plot_list), function(x){assign(use[x], plot_list[[x]], 
                                                 envir = .GlobalEnv)})
+
 tiff(filename = "S:/HelenJohnson/Herpes Zoster/Figures/contact_matrices.tif",
-     width = 800, height = 600)
+     width = 900, height = 900)
 # Current options based on availability of data
-grid.arrange(BE, FI, DE, IE, IT, LU, NL, SK, UK, RS, SI)
+grid_arrange_shared_legend(BE + no_x_axis, FI + no_axes, DE + no_axes, 
+                           IE + no_x_axis, IT + no_axes, LU + no_axes,
+                           NL + no_x_axis, SK + no_axes, UK + no_axes, 
+                           RS, SI + no_y_axis,
+                           ncol = 3, nrow = 4)
 while(!is.null(dev.list())) dev.off()
 
 ## Plot population
@@ -422,7 +463,12 @@ plot_pop <- function(code, ...){
                        y = popSize)) +
     geom_col(width = 1) +
     labs(y = "Population", x = "Age", title = code) +
-    coord_flip() 
+    scale_y_continuous(breaks = seq(2500, 3e05, 5000),
+                       labels = seq(2500, 3e05, 5000)) +
+    ylim(0, 3e05) +
+    coord_flip() +
+    theme(plot.title = element_text(hjust = 0.5), # Ensure centred titles
+                       title = element_text(family = "serif"))
 }
 plot_list <- lapply(use, plot_pop)
 lapply(seq_along(plot_list), function(x){assign(use[x], plot_list[[x]], 
@@ -431,5 +477,9 @@ lapply(seq_along(plot_list), function(x){assign(use[x], plot_list[[x]],
 tiff(filename = "S:/HelenJohnson/Herpes Zoster/Figures/population.tif",
      width = 800, height = 600)
 # Current options based on availability of data
-grid.arrange(BE, FI, DE, IE, IT, LU, NL, SK, UK, RS, SI)
+grid.arrange(BE + no_x_axis, FI + no_axes, DE + no_axes, 
+             IE + no_x_axis, IT + no_axes, LU + no_axes,
+             NL + no_x_axis, SK + no_axes, UK + no_axes, 
+             RS, SI + no_y_axis,
+             ncol = 3, nrow = 4)
 while(!is.null(dev.list())) dev.off()
