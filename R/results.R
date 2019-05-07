c1b7efbfc7ff5dd12611abbe685361c77ef0b699
mariabnd <33788128+mariabnd@users.noreply.github.com>
2019-05-07 11:35:09 +0200

    Use save results rather than re-running them every time

--- R/results.R
+++ R/results.R
@@ -9,7 +9,8 @@ library(gridExtra)
 get_results <- function(code, ...){
   get_data(code)
   #source("https://raw.githubusercontent.com/EU-ECDC/HerpesZosterModel/master/R/MCMC.r")
-  source("MCMC.r") # Use the local version
+  #source("MCMC.r") # Use the local version
+  source("https://raw.githubusercontent.com/EU-ECDC/HerpesZosterModel/additional_plots/R/MCMC.r")
   return(list(code = code,
               dispProp = dispProp,
               mcmcOutput = mcmcOutput,
@@ -28,29 +29,31 @@ get_results <- function(code, ...){
               param0 = param0,
               param1 = param1,
               propFac =	propFac,
-              scaleDisp	= scaleDisp))
+              scaleDisp	= scaleDisp,
+              htab = htab))
 }
-# TODO optimise such that results are only retrieved once - save in a list perhaps
-# Should set a seed also?
+res <- get_results("IT")
+# Check we got what we wanted
+str(res)
 
 library(parallel)
 res <- mclapply(use, get_results)
 
-plot_list <- mclapply(use, function(code){get_results(code)
+plot_list <- mclapply(1 : length(use), function(i){
   ggplot() +
-    geom_line(data = summaryFoI, mapping = aes(x = age, y = midFoi),
+    geom_line(data = res[[i]]$summaryFoI, mapping = aes(x = age, y = midFoi),
               size = 0.01, alpha = 0.8) +
-    geom_ribbon(data = summaryFoI, mapping = aes(x = age, ymin = lower, ymax = upper),
+    geom_ribbon(data = res[[i]]$summaryFoI, mapping = aes(x = age, ymin = lower, ymax = upper),
                 fill = "blue", alpha = 0.2) +
-    geom_line(data = summaryPrev, mapping = aes(x = age, y = midPrev),
+    geom_line(data = res[[i]]$summaryPrev, mapping = aes(x = age, y = midPrev),
               size = 0.01, alpha = 0.8) +
-    geom_ribbon(data = summaryPrev, mapping = aes(x = age, ymin = lower, ymax = upper),
+    geom_ribbon(data = res[[i]]$summaryPrev, mapping = aes(x = age, ymin = lower, ymax = upper),
                 fill = "blue", alpha = 0.2) +
     ylim(0, 1) +
-    geom_point(data = as.data.frame(cbind(age = as.numeric(row.names(htab)), 
-                                          prop = htab[, 2] / rowSums(htab),
+    geom_point(data = as.data.frame(cbind(age = as.numeric(row.names(res[[i]]$htab)), 
+                                          prop = res[[i]]$htab[, 2] / rowSums(res[[i]]$htab),
                                           tot = rowSums(htab))),
-               mapping = aes(x = age, y = prop, size = tot),
+               mapping = aes(x = age, y = prop, size = res[[i]]$htab),
                pch = 1) +
     labs(y = "", size = "number of samples", title = code) +
     scale_size_continuous(limits = c(1, 500),
@@ -70,9 +73,8 @@ while(!is.null(dev.list())) dev.off()
 
 # Plot rates
 # First combine the values for all the countries
-tmp <- sapply(use, function(code){
-  get_results(code)
-  return(as.data.frame(cbind(sampledR, ID = rep(code, dim(sampledR)[1]))))})
+tmp <- sapply(1 : length(use), function(i){
+  return(as.data.frame(cbind(res[[i]]$sampledR, ID = rep(code, dim(sampledR)[1]))))})
 # Rework data into a format that is useful for plotting
 tmp <- do.call(rbind, split(tmp, rep(1 : ncol(tmp), each = nrow(tmp))))
 library(tidyr)
@@ -109,20 +111,18 @@ ggplot() +
 while(!is.null(dev.list())) dev.off()
 
 # Save values
-save_vals <- function(code){
-  get_results(code)
-  return(c(code,
-           propFac,
-           prior,
-           dispProp,
-           nIter,
-           acc.rate,
-           ESS))
+save_vals <- function(i){
+  return(c(res[[i]]$propFac,
+           res[[i]]$prior,
+           res[[i]]$dispProp,
+           res[[i]]$nIter,
+           res[[i]]$acc.rate,
+           res[[i]]$ESS))
 }
 
 library(XLConnect)
-sapply(use, function(code){
+sapply(1 : length(use), function(i){
   mcmc_save <- loadWorkbook("MCMC settings.xlsx")
-  appendWorksheet(mcmc_save, t(save_vals(code)), sheet = 1)
+  appendWorksheet(mcmc_save, t(c(use[i], save_vals(code))), sheet = 1)
   saveWorkbook(mcmc_save)
 })
